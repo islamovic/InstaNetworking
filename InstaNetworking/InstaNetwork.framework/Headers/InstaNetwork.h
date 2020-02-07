bf195b03c13f807a685ddb6ffa79ea448c4b81ce
islamovic <islam.abo.elsoud@gmail.com>
2020-02-07 03:11:51 +0200

    add closed source dynamic framework

---
+++ InstaNetworking/InstaNetwork.framework/Headers/InstaNetwork.h
@@ -0,0 +1,19 @@
+//
+//  InstaNetwork.h
+//  InstaNetwork
+//
+//  Created by Islam on 2/7/20.
+//  Copyright © 2020 App Lineup. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InstaNetwork.
+FOUNDATION_EXPORT double InstaNetworkVersionNumber;
+
+//! Project version string for InstaNetwork.
+FOUNDATION_EXPORT const unsigned char InstaNetworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InstaNetwork/PublicHeader.h>
+
+
